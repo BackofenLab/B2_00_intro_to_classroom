adcf94daf361d4dfadaa1e24ae5bf050bd5ef005
Alexander Mitrofanov <46710688+Alexander-Mitrofanov@users.noreply.github.com>
2023-04-15 13:53:05 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,35 +4,30 @@ import pytest
 class CustomReport:
     def pytest_terminal_summary(self, terminalreporter):
         tr = terminalreporter
-        passed_tests = {}
+        passed_functions = set()
         failed_functions = set()
 
         for rep in tr.getreports(''):
             func_name = rep.nodeid.split("::")[1]
+
             if rep.passed:
-                passed_tests[func_name] = passed_tests.get(func_name, 0) + 1
+                passed_functions.add(func_name)
             elif rep.failed:
                 failed_functions.add(func_name)
-                if func_name in passed_tests:
-                    del passed_tests[func_name]
+                passed_functions.discard(func_name)
 
-        for func_name in passed_tests:
+                tr.write_line(f"Test failed for {rep.nodeid}", red=True)
+
+                for prop in rep.user_properties:
+                    if prop[0] == "input_values":
+                        tr.write_line(f"Input variables are: {prop[1]}", red=True)
+                    if prop[0] == "expected_result":
+                        tr.write_line(f"Expected answer: {prop[1]}", red=True)
+                    if prop[0] == "actual_result":
+                        tr.write_line(f"Your answer: {prop[1]}", red=True)
+
+        for func_name in passed_functions:
             tr.write_line(f"All tests passed for the function {func_name}", green=True)
-
-        if failed_functions:
-            for rep in tr.getreports(''):
-                if rep.failed and rep.nodeid.split("::")[1] in failed_functions:
-                    tr.write_line(f"Test failed for {rep.nodeid}", red=True)
-
-                    for prop in rep.user_properties:
-                        if prop[0] == "input_values":
-                            tr.write_line(f"Input variables are: {prop[1]}", red=True)
-                        if prop[0] == "expected_result":
-                            tr.write_line(f"Expected answer: {prop[1]}", red=True)
-                        if prop[0] == "actual_result":
-                            tr.write_line(f"Your answer: {prop[1]}", red=True)
-
-                    failed_functions.remove(rep.nodeid.split("::")[1])
 
         # Suppress the default summary output.
         terminalreporter.config.option.disable_test_summary = True
