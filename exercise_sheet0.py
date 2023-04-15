68afc092115c94907c4bbd2ae217629dc3dcb5f5
Alexander Mitrofanov <46710688+Alexander-Mitrofanov@users.noreply.github.com>
2023-04-15 02:32:05 +0200

    replace the intro tasks

--- exercise_sheet0.py
+++ exercise_sheet0.py
@@ -1,35 +1,91 @@
-"""
-Please fill in the student credentials.
-It will help us to provide you with the personal feedback.
-It is also ok to leave them empty if you wish to solve the tasks anonymously.
-"""
-
-first_name = ""
-last_name = ""
-student_id = ""
-
-
-def exercise_1():
+def add_numbers(a, b):
     """
-    Exercise_1
-    Assign the correct True and False values to the corresponding variables"""
-
-    # The Earth is flat.
-    a = None
-
-    # London is the Capital of Great Britain.
-    b = None
-
-    # The 8th season of Game of Thrones was good.
-    c = None
-
-    return a, b, c
+    This function takes two numbers as input and returns their sum.
+    Example:
+        add_numbers(3, 5) -> 8
+    """
+    pass
 
 
-########################################################
-############## Programming tasks #######################
-########################################################
+def subtract_numbers(a, b):
+    """
+    This function takes two numbers as input and returns the result of subtracting the second number from the first number.
+    Example:
+        subtract_numbers(7, 2) -> 5
+    """
+    pass
 
 
-def factorial(x):
-    return 1
+def multiply_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their product.
+    Example:
+        multiply_numbers(4, 6) -> 24
+    """
+    pass
+
+
+def divide_numbers(a, b):
+    """
+    This function takes two numbers as input and returns the result of dividing the first number by the second number.
+    Handle the case when the second number is zero.
+    Example:
+        divide_numbers(8, 2) -> 4.0
+    """
+    pass
+
+
+def remainder(a, b):
+    """
+    This function takes two numbers as input and returns the remainder when the first number is divided by the second number.
+    Handle the case when the second number is zero.
+    Example:
+        remainder(7, 3) -> 1
+    """
+    pass
+
+
+def exponentiate(base, exponent):
+    """
+    This function takes two numbers as input (a base and an exponent) and returns the result of raising the base to the power of the exponent.
+    Example:
+        exponentiate(2, 3) -> 8
+    """
+    pass
+
+
+def average(numbers):
+    """
+    This function takes a list of numbers as input and returns the average of the numbers in the list.
+    Example:
+        average([1, 2, 3, 4, 5]) -> 3.0
+    """
+    pass
+
+
+def gcd(a, b):
+    """
+    This function takes two numbers as input and returns their greatest common divisor.
+    Example:
+        gcd(14, 28) -> 14
+    """
+    pass
+
+
+def lcm(a, b):
+    """
+    This function takes two numbers as input and returns their least common multiple.
+    Example:
+        lcm(3, 5) -> 15
+    """
+    pass
+
+
+def celsius_to_fahrenheit(celsius):
+    """
+    This function takes a temperature in Celsius as input and returns the equivalent temperature in Fahrenheit.
+    Use the formula: F = (C * 9/5) + 32
+    Example:
+        celsius_to_fahrenheit(25) -> 77.0
+    """
+    pass
