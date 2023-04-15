72b291708277cfe5acb297d6c2f17b27f8eaac00
Alexander Mitrofanov <46710688+Alexander-Mitrofanov@users.noreply.github.com>
2023-04-15 13:15:43 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,42 @@
 import pytest
 
+
+class CustomReport:
+    def pytest_terminal_summary(self, terminalreporter):
+        tr = terminalreporter
+        tests_passed = 0
+        failed_tests = []
+
+        for rep in tr.getreports(''):
+            if rep.passed:
+                tests_passed += 1
+            elif rep.failed:
+                failed_tests.append(rep)
+
+        if tests_passed > 0:
+            tr.write_line(f"All tests passed for {tests_passed} functions.", green=True)
+
+        if failed_tests:
+            for rep in failed_tests[:1]:
+                tr.write_line(f"Test failed for {rep.nodeid}", red=True)
+
+                for prop in rep.user_properties:
+                    if prop[0] == "input_values":
+                        tr.write_line(f"Input variables are: {prop[1]}", red=True)
+                    if prop[0] == "expected_result":
+                        tr.write_line(f"Expected answer: {prop[1]}", red=True)
+                    if prop[0] == "actual_result":
+                        tr.write_line(f"Your answer: {prop[1]}", red=True)
+
+        # Suppress the default summary output.
+        terminalreporter.config.option.disable_test_summary = True
+
+
+def pytest_configure(config):
+    if config.getoption("--custom-report"):
+        config.pluginmanager.register(CustomReport(), "customreport")
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--custom-report",
@@ -7,24 +44,3 @@ def pytest_addoption(parser):
         default=True,
         help="Use custom report format",
     )
-
-def pytest_configure(config):
-    if config.getoption("--custom-report"):
-        config.pluginmanager.register(CustomReport(), "customreport")
-
-class CustomReport:
-
-    def pytest_terminal_summary(self, terminalreporter):
-        tr = terminalreporter
-        passed_tests = tr.stats.get("passed", [])
-        failed_tests = tr.stats.get("failed", [])
-
-        for test in passed_tests:
-            tr.write("\033[32mAll tests passed\033[0m for {}\n".format(test.nodeid))
-
-        for test in failed_tests:
-            tr.write("\033[31mTest failed\033[0m for {}\n".format(test.nodeid))
-            input_vars = test._report.sections[0][1]
-            tr.write("  Input variables are: {}\n".format(input_vars))
-            error_message = test._report.longrepr.reprtraceback.reprentries[-1].reprfileloc.message
-            tr.write("  {}\n".format(error_message))
